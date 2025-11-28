8e6b78f52ee7567dc36537dafcebc4fb67ff1f94
GalimovaAS <alinagz0206@gmail.com>
2025-11-28 16:06:24 +0500

    все видео

---
+++ TodoServerApp/Data/Migrations/20251128103344_Name_TaskItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TodoServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Name_TaskItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    FinishDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TaskItems",
+                columns: new[] { "Id", "CreatedDate", "Description", "FinishDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 11, 28, 15, 33, 44, 557, DateTimeKind.Local).AddTicks(7525), "Описание задачи 1", null, "Задача 1" },
+                    { 2, new DateTime(2025, 11, 28, 15, 33, 44, 557, DateTimeKind.Local).AddTicks(7539), "Описание задачи 2", null, "Задача 2" },
+                    { 3, new DateTime(2025, 11, 28, 15, 33, 44, 557, DateTimeKind.Local).AddTicks(7540), "Описание задачи 3", null, "Задача 3" },
+                    { 4, new DateTime(2025, 11, 28, 15, 33, 44, 557, DateTimeKind.Local).AddTicks(7542), "Описание задачи 4", null, "Задача 4" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskItems");
+        }
+    }
+}
---
+++ TodoServerApp/Data/Services/MSSQLDataService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoServerApp.Data.Interfaces;
+
+
+namespace TodoServerApp.Data.Services
+{
+    public class MSSQLDataService(ApplicationDbContext context) : IDataService
+    {
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
+        {
+            return await context.TaskItems.ToListAsync();
+        }
+
+      
+    }
+}
